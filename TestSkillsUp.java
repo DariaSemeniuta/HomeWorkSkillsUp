aa6eff6febb9856fff779d4ff01d2a7595a014de
Daria Semeniuta <dashasemenyuta@gmail.com>
2016-04-19 19:46:01 +0300

    Update TestSkillsUp.java

--- TestSkillsUp.java
+++ TestSkillsUp.java
@@ -1,8 +1,10 @@
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
+       import org.openqa.selenium.By;
+        import org.openqa.selenium.NoSuchElementException;
+        import org.openqa.selenium.WebElement;
+        import org.openqa.selenium.interactions.Actions;
+
+import java.util.List;
 
 /**
  * Created by Daria
@@ -14,13 +16,23 @@ public class TestSkillsUp extends BaseTest{
     private By coachPresent1=By.xpath("//div[@class='team-list clearfix']/div[@class='member']/a/span[contains(text(),'Александр Галковский')]");
     private By coachPresent2=By.xpath("//div[@class='team-list clearfix']/div[@class='member']/a/span[contains(text(),'Михаил Чокан')]");
     private By coachAbsent=By.xpath("//div[@class='team-list clearfix']/div[@class='member']/a/span[contains(text(),'Артем Карпов')]");
+    private By path=By.className("name");
     private String nameCoach1="Александр Галковский";
     private String nameCoach2="Михаил Чокан";
+    private String nameCoach3="Артем Карпов";
     private WebElement element;
     private Actions action;
 
+    private String findCoaches () {
+        List<WebElement> element = driver.findElements(path);
+        String name="";
+        for (WebElement i: element)
+            name= name+i.getText()+" ";
+        return name;
+    }
+
     @Test
-    public void test1() throws Exception{
+    public void testGalkovskiy() throws Exception{
         setUp(startPage);
         action = new Actions(driver);
         element=driver.findElement(aboutUs);
@@ -28,7 +40,8 @@ public class TestSkillsUp extends BaseTest{
         element=driver.findElement(ourTeam);
         action.moveToElement(element);
         action.click().build().perform();
-        element=driver.findElement(coachPresent1);
+        assertTrue("Тренера Галковского нет!",findCoaches().contains(nameCoach1));
+        /*element=driver.findElement(coachPresent1);
         assertEquals(nameCoach1,element.getText());
         element=driver.findElement(coachPresent2);
         assertEquals(nameCoach2,element.getText());
@@ -37,8 +50,28 @@ public class TestSkillsUp extends BaseTest{
         }
         catch (NoSuchElementException e){
             System.out.println("Артема нет в списке тренеров");
-        }
+        }*/
+    }
+    public void testChokan() throws Exception{
+        setUp(startPage);
+        action = new Actions(driver);
+        element=driver.findElement(aboutUs);
+        action.moveToElement(element);
+        element=driver.findElement(ourTeam);
+        action.moveToElement(element);
+        action.click().build().perform();
+        assertTrue("Тренера Галковского нет!",findCoaches().contains(nameCoach2));
 
+    }
+    public void testArtem() throws Exception{
+        setUp(startPage);
+        action = new Actions(driver);
+        element=driver.findElement(aboutUs);
+        action.moveToElement(element);
+        element=driver.findElement(ourTeam);
+        action.moveToElement(element);
+        action.click().build().perform();
+        assertFalse("Тренер Артем есть!",findCoaches().contains(nameCoach3));
 
     }
 }
